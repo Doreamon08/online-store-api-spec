b1f7aa15f4261961120926cc6edf3b87ab4b5e70
rabbie <rabbie@student.21-school.ru>
2025-04-15 14:35:50 +0300

    add orders.yaml

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,12 @@
+dependencies {
+    implementation("com.squareup.okhttp3:okhttp:4.9.3")
+    implementation("com.squareup.moshi:moshi:1.13.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.13.0")}
+
 plugins {
     id("org.openapi.generator") version "7.4.0"
+    id("maven-publish")
+    kotlin("jvm") version "1.9.0"
 }
 
 repositories {
@@ -7,15 +14,44 @@ repositories {
 }
 
 openApiGenerate {
-    generatorName.set("kotlin")  // Указываем тип генератора
+    generatorName.set("kotlin")
     inputSpec.set("$rootDir/src/main/resources/api/online-store.yaml")
     outputDir.set("$buildDir/generated")
     apiPackage.set("com.example.api")
     modelPackage.set("com.example.model")
     invokerPackage.set("com.example.invoker")
     configOptions.set(
-        mapOf(
-            "dateLibrary" to "java8"
-        )
+        mapOf("dateLibrary" to "java8")
     )
+}
+
+java {
+    sourceSets["main"].java.srcDir("$buildDir/generated/src/main/kotlin")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "com.doreamon"
+            artifactId = "online-store-api-spec"
+            version = "1.0.0"
+            from(components["java"])
+        }
+    }
+}
+
+kotlin {
+    sourceSets["main"].kotlin.srcDir("build/generated/src/main/kotlin")
+}
+
+tasks.named("publishToMavenLocal") {
+    dependsOn("openApiGenerate")
+}
+
+tasks.named<Jar>("jar") {
+    from("$buildDir/generated/src/main/kotlin")
+}
+
+tasks.named("compileKotlin") {
+    dependsOn("openApiGenerate")
 }
